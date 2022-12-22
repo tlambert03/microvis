bd2bde3476707cb85389eeb59e94fed1a6b8e963
alisterburt <alisterburt@gmail.com>
2022-12-22 10:48:15 +0000

    _native -> _vispy_node

--- src/microvis/backend/vispy/_camera.py
+++ src/microvis/backend/vispy/_camera.py
@@ -14,37 +14,37 @@ from ._node import Node
 class Camera(Node, camera.CameraBackend):
     """Adaptor for vispy camera."""
 
-    _vispy_camera: scene.cameras.BaseCamera
+    _vispy_node: scene.cameras.BaseCamera
 
     def __init__(self, camera: camera.Camera, **backend_kwargs: Any) -> None:
         backend_kwargs.setdefault("flip", (0, 1, 0))  # Add to core schema?
         # backend_kwargs.setdefault("aspect", 1)
         cam = scene.cameras.make_camera(str(camera.type), **backend_kwargs)
-        self._vispy_camera = cam
+        self._vispy_node = cam
 
     def _viz_set_zoom(self, zoom: float) -> None:
         if (view_size := self._view_size()) is None:
             return
         scale = np.array(view_size) / zoom
-        if hasattr(self._vispy_camera, "scale_factor"):
-            self._vispy_camera.scale_factor = np.min(scale)
+        if hasattr(self._vispy_node, "scale_factor"):
+            self._vispy_node.scale_factor = np.min(scale)
         else:
             # Set view rectangle, as left, right, width, height
-            corner = np.subtract(self._vispy_camera.center[:2], scale / 2)
-            self._vispy_camera.rect = tuple(corner) + tuple(scale)
+            corner = np.subtract(self._vispy_node.center[:2], scale / 2)
+            self._vispy_node.rect = tuple(corner) + tuple(scale)
 
     def _viz_set_center(self, arg: tuple[float, ...]) -> None:
-        self._vispy_camera.center = arg[::-1]  # TODO
-        self._vispy_camera.view_changed()
+        self._vispy_node.center = arg[::-1]  # TODO
+        self._vispy_node.view_changed()
 
     def _viz_set_type(self, arg: CameraType) -> None:
-        if not isinstance(self._vispy_camera.parent, scene.ViewBox):
+        if not isinstance(self._vispy_node.parent, scene.ViewBox):
             raise TypeError("Camera must be attached to a ViewBox")
-        self._vispy_camera.parent.camera = str(arg)
+        self._vispy_node.parent.camera = str(arg)
 
     def _view_size(self) -> tuple[float, float] | None:
         """Return the size of first parent viewbox in pixels."""
-        obj = self._vispy_camera
+        obj = self._vispy_node
         while (obj := obj.parent) is not None:
             if isinstance(obj, scene.ViewBox):
                 return cast("tuple[float, float]", obj.size)
--- src/microvis/backend/vispy/_scene.py
+++ src/microvis/backend/vispy/_scene.py
@@ -10,9 +10,9 @@ from ._node import Node
 
 class Scene(Node):
     def __init__(self, scene: core.Scene, **backend_kwargs: Any) -> None:
-        self._native = SubScene(**backend_kwargs)
-        self._native._clipper = Clipper()
-        self._native.clip_children = True
+        self._vispy_node = SubScene(**backend_kwargs)
+        self._vispy_node._clipper = Clipper()
+        self._vispy_node.clip_children = True
 
         for node in scene.children:
             node.backend_adaptor()  # create backend adaptor if it doesn't exist
