50db959caf8634955384e110b12fa3302289f176
alisterburt <alisterburt@gmail.com>
2022-12-22 10:40:14 +0000

    camera

--- src/microvis/backend/vispy/_camera.py
+++ src/microvis/backend/vispy/_camera.py
@@ -14,37 +14,37 @@ from ._node import Node
 class Camera(Node, camera.CameraBackend):
     """Adaptor for vispy camera."""
 
-    _native: scene.cameras.BaseCamera
+    _vispy_camera: scene.cameras.BaseCamera
 
     def __init__(self, camera: camera.Camera, **backend_kwargs: Any) -> None:
         backend_kwargs.setdefault("flip", (0, 1, 0))  # Add to core schema?
         # backend_kwargs.setdefault("aspect", 1)
         cam = scene.cameras.make_camera(str(camera.type), **backend_kwargs)
-        self._native = cam
+        self._vispy_camera = cam
 
     def _viz_set_zoom(self, zoom: float) -> None:
         if (view_size := self._view_size()) is None:
             return
         scale = np.array(view_size) / zoom
-        if hasattr(self._native, "scale_factor"):
-            self._native.scale_factor = np.min(scale)
+        if hasattr(self._vispy_camera, "scale_factor"):
+            self._vispy_camera.scale_factor = np.min(scale)
         else:
             # Set view rectangle, as left, right, width, height
-            corner = np.subtract(self._native.center[:2], scale / 2)
-            self._native.rect = tuple(corner) + tuple(scale)
+            corner = np.subtract(self._vispy_camera.center[:2], scale / 2)
+            self._vispy_camera.rect = tuple(corner) + tuple(scale)
 
     def _viz_set_center(self, arg: tuple[float, ...]) -> None:
-        self._native.center = arg[::-1]  # TODO
-        self._native.view_changed()
+        self._vispy_camera.center = arg[::-1]  # TODO
+        self._vispy_camera.view_changed()
 
     def _viz_set_type(self, arg: CameraType) -> None:
-        if not isinstance(self._native.parent, scene.ViewBox):
+        if not isinstance(self._vispy_camera.parent, scene.ViewBox):
             raise TypeError("Camera must be attached to a ViewBox")
-        self._native.parent.camera = str(arg)
+        self._vispy_camera.parent.camera = str(arg)
 
     def _view_size(self) -> tuple[float, float] | None:
         """Return the size of first parent viewbox in pixels."""
-        obj = self._native
+        obj = self._vispy_camera
         while (obj := obj.parent) is not None:
             if isinstance(obj, scene.ViewBox):
                 return cast("tuple[float, float]", obj.size)
