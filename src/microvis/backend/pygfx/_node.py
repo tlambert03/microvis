a3f2d8f43806901ec3b620699551bf6bbc39e4b7
alisterburt <alisterburt@gmail.com>
2022-12-24 12:48:48 +0000

    node viz -> vis

--- src/microvis/backend/pygfx/_node.py
+++ src/microvis/backend/pygfx/_node.py
@@ -18,38 +18,38 @@ class Node(core_node.NodeAdaptorProtocol):
     _geometry: Geometry
     _name: str
 
-    def _viz_get_native(self) -> Any:
+    def _vis_get_native(self) -> Any:
         return self._native
 
-    def _viz_set_name(self, arg: str) -> None:
+    def _vis_set_name(self, arg: str) -> None:
         # not sure pygfx has a name attribute...
         # TODO: for that matter... do we need a name attribute?
         # Could this be entirely managed on the model side/
         self._name = arg
 
-    def _viz_set_parent(self, arg: core_node.Node | None) -> None:
+    def _vis_set_parent(self, arg: core_node.Node | None) -> None:
         raise NotImplementedError
 
-    def _viz_set_children(self, arg: list[core_node.Node]) -> None:
-        # This is probably redundant with _viz_add_node
+    def _vis_set_children(self, arg: list[core_node.Node]) -> None:
+        # This is probably redundant with _vis_add_node
         # could maybe be a clear then add *arg
         raise NotImplementedError
 
-    def _viz_set_visible(self, arg: bool) -> None:
+    def _vis_set_visible(self, arg: bool) -> None:
         self._native.visible = arg
 
-    def _viz_set_opacity(self, arg: float) -> None:
+    def _vis_set_opacity(self, arg: float) -> None:
         self._material.opacity = arg
 
-    def _viz_set_order(self, arg: int) -> None:
+    def _vis_set_order(self, arg: int) -> None:
         self._native.render_order = arg
 
-    def _viz_set_interactive(self, arg: bool) -> None:
+    def _vis_set_interactive(self, arg: bool) -> None:
         # this one requires knowledge of the controller
         raise NotImplementedError
 
-    def _viz_set_transform(self, arg: Transform) -> None:
+    def _vis_set_transform(self, arg: Transform) -> None:
         self._native.matrix = arg.matrix  # TODO: check this
 
-    def _viz_add_node(self, node: core_node.Node) -> None:
+    def _vis_add_node(self, node: core_node.Node) -> None:
         self._native.add(node.native)
